75e709e7570cae0d3d287511ee30e720d49392de
Geoffroy Streit <geoffroy.streit@gmail.com>
2023-11-21 18:30:00 +0100

    Amélioration Ex 1 : Plus d'infos système

--- Python_web/01_flask_installation/check_flask.py
+++ Python_web/01_flask_installation/check_flask.py
@@ -1,12 +1,18 @@
 import flask
+import sys
+import platform
 
 # 21/11/2023 - Geoffroy
-# Premier jet pour vérifier que l'équipement est prêt.
-# C'est un peu comme vérifier sa fiche de perso avant le début de la campagne.
-# Si Flask répond présent, on peut partir à l'aventure.
+# Update : Ajout de quelques détails sur le système.
+# C'est toujours bon de savoir sur quel terrain on évolue (OS, version Python...).
+# Comme vérifier la météo avant de sortir de l'auberge.
 
 def check_installation():
-    print(f"Flask version: {flask.__version__}")
+    print(f"--- Rapport de reconnaissance ---")
+    print(f"Système      : {platform.system()} {platform.release()}")
+    print(f"Python       : {sys.version.split()[0]}")
+    print(f"Flask        : {flask.__version__}")
+    print("---------------------------------")
     print("Moteur démarré. Le 'Dungeon Master' Python est prêt.")
 
 if __name__ == "__main__":
