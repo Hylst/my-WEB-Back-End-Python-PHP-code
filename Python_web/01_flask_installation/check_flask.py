2c4778c8f7c9d38c9ad33ac9d426ba42b300df57
Geoffroy Streit <geoffroy.streit@gmail.com>
2023-11-21 22:00:00 +0100

    Upgrade Ex 1 : Outil de diagnostic système complet

--- Python_web/01_flask_installation/check_flask.py
+++ Python_web/01_flask_installation/check_flask.py
@@ -1,19 +1,50 @@
 import flask
 import sys
 import platform
+import os
+import shutil
 
 # 21/11/2023 - Geoffroy
-# Update : Ajout de quelques détails sur le système.
-# C'est toujours bon de savoir sur quel terrain on évolue (OS, version Python...).
-# Comme vérifier la météo avant de sortir de l'auberge.
+# Update Soirée : J'ai creusé un peu plus.
+# Un bon aventurier vérifie toujours son inventaire ET son environnement.
+# Ce script scanne tout : version, environnement virtuel, espace disque...
+# C'est peut-être overkill, mais j'aime bien savoir où je mets les pieds.
 
-def check_installation():
-    print(f"--- Rapport de reconnaissance ---")
-    print(f"Système      : {platform.system()} {platform.release()}")
-    print(f"Python       : {sys.version.split()[0]}")
-    print(f"Flask        : {flask.__version__}")
-    print("---------------------------------")
-    print("Moteur démarré. Le 'Dungeon Master' Python est prêt.")
+def check_system():
+    print(f"\n--- [ SCAN SYSTÈME ] ---")
+    print(f"OS           : {platform.system()} {platform.release()} ({platform.version()})")
+    print(f"Machine      : {platform.machine()}")
+    print(f"Processeur   : {platform.processor()}")
+    
+    # Check disque pour éviter les surprises
+    total, used, free = shutil.disk_usage(".")
+    print(f"Espace Disque: {free // (2**30)} Go libres / {total // (2**30)} Go total")
+
+def check_python_env():
+    print(f"\n--- [ ENVIRONNEMENT PYTHON ] ---")
+    print(f"Version      : {sys.version.split()[0]}")
+    print(f"Exécutable   : {sys.executable}")
+    
+    # Vérification VENV
+    in_venv = (sys.prefix != sys.base_prefix)
+    status = "ACTIVÉ ✅" if in_venv else "DÉSACTIVÉ ⚠️ (Attention aux conflits)"
+    print(f"Virtual Env  : {status}")
+    
+    # Path
+    print(f"PYTHONPATH   : {os.environ.get('PYTHONPATH', 'Non défini')}")
+
+def check_flask():
+    print(f"\n--- [ MODULE FLASK ] ---")
+    try:
+        print(f"Version      : {flask.__version__}")
+        print(f"Chemin       : {os.path.dirname(flask.__file__)}")
+        print("Statut       : OPÉRATIONNEL 🚀")
+    except Exception as e:
+        print(f"Statut       : ERREUR CRITIQUE 💀 ({e})")
 
 if __name__ == "__main__":
-    check_installation()
+    print("Initialisation des protocoles de diagnostic...")
+    check_system()
+    check_python_env()
+    check_flask()
+    print("\n[ FIN DU RAPPORT ]")
