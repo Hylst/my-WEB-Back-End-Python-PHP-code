a354f56acb9472a702d62d9ce946b887d8376b7d
Geoffroy Streit <geoffroy.streit@gmail.com>
2023-11-22 20:00:00 +0100

    Amélioration Ex 2 : Route de statut

--- Python_web/02_flask_hello_world/app.py
+++ Python_web/02_flask_hello_world/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, Response
+from flask import Flask, Response, jsonify
 
 # 22/11/2023 - Geoffroy
 # Le fameux Hello World.
 # C'est un peu le "Roll for Initiative" du dev web.
 # On vérifie juste que le serveur répond correctement.
+# Update : Ajout d'une route /status pour vérifier que tout est vert.
 
 app = Flask(__name__)
 
@@ -11,6 +12,11 @@ app = Flask(__name__)
 def hello():
     # J'utilise Response pour être explicite, comme quand on déclare ses actions au MJ.
     return Response("Hello World! Le serveur Flask est en ligne.", mimetype='text/plain')
+
+@app.route('/status')
+def status():
+    # Petit check rapide en JSON.
+    return jsonify({"server": "up", "level": 1, "class": "Flask"})
 
 if __name__ == "__main__":
     # Debug=True, c'est comme jouer avec l'écran du MJ baissé pour apprendre.
