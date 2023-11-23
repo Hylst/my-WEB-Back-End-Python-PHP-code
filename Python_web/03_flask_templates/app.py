8daf993fae23f7a8051df6587f812026f2b710fb
Geoffroy Streit <geoffroy.streit@gmail.com>
2023-11-23 22:00:00 +0100

    Upgrade Ex 3 : Static CSS, NavMenu et Complex Data

--- Python_web/03_flask_templates/app.py
+++ Python_web/03_flask_templates/app.py
@@ -1,17 +1,30 @@
 from flask import Flask, render_template
 
 # 23/11/2023 - Geoffroy
-# Les templates. Très important.
-# Ça permet de ne pas mélanger les règles (Python) et le plateau de jeu (HTML).
-# Une bonne séparation des préoccupations, comme dans une équipe équilibrée (Tank/Healer/DPS).
+# Les templates V2 - Avec du style (CSS) !
+# J'ai ajouté un dossier static/ pour les assets.
+# Et on passe des structures de données complexes (listes de dictionnaires) pour tester les boucles Jinja.
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     user_name = "Voyageur"
-    # On passe des variables au template, comme on distribue des cartes aux joueurs.
     return render_template('home.html', name=user_name)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/team')
+def team():
+    # Simulation d'une équipe récupérée en base
+    members = [
+        {"name": "Geoffroy", "role": "Mage Codeur", "level": 5},
+        {"name": "Flask", "role": "Framework Agile", "level": 10},
+        {"name": "Jinja", "role": "Moteur de Rendu", "level": 8}
+    ]
+    return render_template('team.html', members=members)
 
 if __name__ == "__main__":
     app.run(debug=True)
