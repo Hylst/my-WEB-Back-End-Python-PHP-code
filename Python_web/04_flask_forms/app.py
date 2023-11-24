b77814e698bdf56b8ae864e60f3c5108302e2eab
Geoffroy Streit <geoffroy.streit@gmail.com>
2023-11-24 16:00:00 +0100

    Amélioration Ex 4 : Validation et messages d'erreur

--- Python_web/04_flask_forms/app.py
+++ Python_web/04_flask_forms/app.py
@@ -10,14 +10,21 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def contact():
     message_sent = False
+    error = None
+    
     if request.method == 'POST':
         # On récupère les données du formulaire
         nom = request.form.get('nom')
         msg = request.form.get('message')
-        print(f"Nouveau message de {nom} : {msg}")
-        message_sent = True
         
-    return render_template('form.html', success=message_sent)
+        # Validation basique
+        if not nom or not msg:
+            error = "Hé oh, il faut remplir tous les champs !"
+        else:
+            print(f"Nouveau message de {nom} : {msg}")
+            message_sent = True
+        
+    return render_template('form.html', success=message_sent, error=error)
 
 if __name__ == "__main__":
     app.run(debug=True)
