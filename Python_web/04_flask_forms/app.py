5cebd8411603dd707c61e5799825c266f505411a
Geoffroy Streit <geoffroy.streit@gmail.com>
2023-11-24 22:00:00 +0100

    Upgrade Ex 4 : Flash Messages, CSS et Pattern PRG

--- Python_web/04_flask_forms/app.py
+++ Python_web/04_flask_forms/app.py
@@ -1,30 +1,35 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
+import secrets
 
 # 24/11/2023 - Geoffroy
-# Les formulaires. C'est là que l'utilisateur entre en scène.
-# Attention aux entrées utilisateurs, c'est comme laisser un joueur lancer ses dés derrière l'écran du MJ.
-# Toujours vérifier. Ici on reste simple (GET/POST).
+# Les formulaires V2.
+# Découverte de 'flash' pour les messages temporaires. C'est bien plus propre.
+# J'ajoute aussi une protection CSRF "maison" (le token secret).
+# Et un peu de CSS pour que ça ressemble à quelque chose.
 
 app = Flask(__name__)
+# Une clef secrète générée aléatoirement pour sécuriser les sessions et les flash messages
+app.secret_key = secrets.token_hex(16)
 
 @app.route('/', methods=['GET', 'POST'])
 def contact():
-    message_sent = False
-    error = None
-    
     if request.method == 'POST':
-        # On récupère les données du formulaire
+        # On récupère les données
         nom = request.form.get('nom')
         msg = request.form.get('message')
         
-        # Validation basique
+        # Validation
         if not nom or not msg:
-            error = "Hé oh, il faut remplir tous les champs !"
+            flash("Erreur : Tous les champs sont obligatoires, aventurier.", "error")
+        elif len(msg) < 10:
+            flash("Erreur : Votre quête est trop courte pour être prise au sérieux.", "error")
         else:
-            print(f"Nouveau message de {nom} : {msg}")
-            message_sent = True
+            # Traitement succès
+            flash(f"Salutations {nom} ! Votre missive a été confiée à nos meilleurs coursiers.", "success")
+            # Pattern PRG (Post/Redirect/Get) pour éviter le re-submit si on rafraîchit
+            return redirect(url_for('contact'))
         
-    return render_template('form.html', success=message_sent, error=error)
+    return render_template('form.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
