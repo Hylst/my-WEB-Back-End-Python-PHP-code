820627cb205f03960bbb74021d79eab92005cfbd
Geoffroy Streit <geoffroy.streit@gmail.com>
2023-11-24 10:15:00 +0100

    Exercice 4 : Formulaires (GET/POST)

---
+++ Python_web/04_flask_forms/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+# 24/11/2023 - Geoffroy
+# Les formulaires. C'est là que l'utilisateur entre en scène.
+# Attention aux entrées utilisateurs, c'est comme laisser un joueur lancer ses dés derrière l'écran du MJ.
+# Toujours vérifier. Ici on reste simple (GET/POST).
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def contact():
+    message_sent = False
+    if request.method == 'POST':
+        # On récupère les données du formulaire
+        nom = request.form.get('nom')
+        msg = request.form.get('message')
+        print(f"Nouveau message de {nom} : {msg}")
+        message_sent = True
+        
+    return render_template('form.html', success=message_sent)
+
+if __name__ == "__main__":
+    app.run(debug=True)
