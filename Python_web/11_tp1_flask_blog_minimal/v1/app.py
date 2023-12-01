ada6a944e64f932f2d98b7e8c28b828b6f63f43f
Geoffroy Streit <geoffroy.streit@gmail.com>
2023-12-01 22:00:00 +0100

    Upgrade TP1 V1 : Catégories et compteur de vues

--- Python_web/11_tp1_flask_blog_minimal/v1/app.py
+++ Python_web/11_tp1_flask_blog_minimal/v1/app.py
@@ -1,54 +1,91 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from datetime import datetime
+import secrets
 
 # 01/12/2023 - Geoffroy
-# TP1 - Version 1 : Le Blog Minimaliste.
-# C'est un grand classique. On commence petit.
-# Pour l'instant, les articles sont stockés en RAM (liste Python).
-# Si le serveur restart, pouf, plus d'articles. C'est un blog éphémère, poétique non ?
+# TP1 V1 - Blog Amélioré
+# Structure plus propre avec des vrais templates séparés.
+# Ajout de catégories et de la gestion des erreurs.
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)
 
-# Notre base de données très volatile.
+# Base de données en mémoire
 articles = [
     {
         "id": 1,
         "titre": "Bienvenue sur mon Codex",
-        "contenu": "Ceci est le tout premier article. Flask est fun !",
-        "date": "2023-12-01"
+        "contenu": "Ceci est le tout premier article. Flask est fun ! Je découvre petit à petit les joies du développement web avec Python.",
+        "auteur": "Geoffroy",
+        "categorie": "General",
+        "date": "2023-12-01",
+        "vues": 42
+    },
+    {
+        "id": 2,
+        "titre": "Les Secrets de Jinja2",
+        "contenu": "Le moteur de templates Jinja2 est vraiment puissant. Héritage, macros, filtres... C'est comme apprendre un nouveau sort.",
+        "auteur": "Geoffroy",
+        "categorie": "Tutoriel",
+        "date": "2023-12-01",
+        "vues": 15
     }
 ]
 
+CATEGORIES = ["General", "Tutoriel", "Aventure", "Réflexion"]
+
+def get_next_id():
+    return max(a['id'] for a in articles) + 1 if articles else 1
+
 @app.route('/')
 def index():
-    return render_template('index.html', articles=articles)
+    cat_filter = request.args.get('cat')
+    filtered = articles
+    if cat_filter:
+        filtered = [a for a in articles if a['categorie'] == cat_filter]
+    return render_template('index.html', 
+        articles=sorted(filtered, key=lambda x: x['date'], reverse=True),
+        categories=CATEGORIES,
+        current_cat=cat_filter
+    )
+
+@app.route('/article/<int:id>')
+def view_article(id):
+    article = next((a for a in articles if a['id'] == id), None)
+    if not article:
+        flash("Article introuvable", "error")
+        return redirect(url_for('index'))
+    article['vues'] += 1
+    return render_template('article.html', article=article)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        titre = request.form['titre']
-        contenu = request.form['contenu']
-        if titre and contenu:
-            # On génère un ID unique (en supposant qu'on ne supprime rien pour l'instant)
-            new_id = len(articles) + 1
+        titre = request.form.get('titre', '').strip()
+        contenu = request.form.get('contenu', '').strip()
+        categorie = request.form.get('categorie', 'General')
+        
+        if not titre or not contenu:
+            flash("Titre et contenu obligatoires", "error")
+        else:
             new_article = {
-                "id": new_id,
+                "id": get_next_id(),
                 "titre": titre,
                 "contenu": contenu,
-                "date": datetime.now().strftime("%Y-%m-%d")
+                "auteur": "Geoffroy",
+                "categorie": categorie,
+                "date": datetime.now().strftime("%Y-%m-%d"),
+                "vues": 0
             }
             articles.append(new_article)
-            # Retour à l'accueil
+            flash(f"Article '{titre}' créé !", "success")
             return redirect(url_for('index'))
-    return render_template('add.html')
+    
+    return render_template('add.html', categories=CATEGORIES)
 
-@app.route('/article/<int:id>')
-def view_article(id):
-    # Recherche simple. Pas très opti mais ça passe.
-    article = next((a for a in articles if a['id'] == id), None)
-    if article:
-        return render_template('article.html', article=article)
-    return "Article introuvable (404 - Échec critique de perception)", 404
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
