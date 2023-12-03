54ceb0a9a84b8fb16a0bcff3f766985fccf64564
Geoffroy Streit <geoffroy.streit@gmail.com>
2023-12-03 22:00:00 +0100

    Upgrade TP2 V1 : Priorités et filtres avancés

--- Python_web/12_tp2_django_todolist/v1/todo/models.py
+++ Python_web/12_tp2_django_todolist/v1/todo/models.py
@@ -1,14 +1,38 @@
 from django.db import models
+from django.utils import timezone
 
 # 03/12/2023 - Geoffroy
-# TP2 - Version 1 : La To-Do List.
-# Simple. Efficace.
-# On commence avec un modèle Tache basique.
+# TP2 V1 - Modèle Task amélioré
+# Ajout de priorité et de dates pour un vrai gestionnaire de tâches.
 
 class Task(models.Model):
-    title = models.CharField(max_length=200)
+    PRIORITY_CHOICES = [
+        (1, '🔴 Urgent'),
+        (2, '🟠 Important'),
+        (3, '🟡 Normal'),
+        (4, '🟢 Faible'),
+    ]
+    
+    title = models.CharField(max_length=200, verbose_name="Titre")
+    description = models.TextField(blank=True, verbose_name="Description")
     completed = models.BooleanField(default=False)
+    priority = models.IntegerField(choices=PRIORITY_CHOICES, default=3)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    due_date = models.DateField(null=True, blank=True, verbose_name="Échéance")
+    
+    class Meta:
+        ordering = ['completed', 'priority', 'due_date']
+    
     def __str__(self):
-        return self.title
+        status = "✅" if self.completed else "⬜"
+        return f"{status} {self.title}"
+    
+    @property
+    def is_overdue(self):
+        if self.due_date and not self.completed:
+            return self.due_date < timezone.now().date()
+        return False
+    
+    @property
+    def priority_label(self):
+        return dict(self.PRIORITY_CHOICES).get(self.priority, "Normal")
--- Python_web/12_tp2_django_todolist/v1/todo/views.py
+++ Python_web/12_tp2_django_todolist/v1/todo/views.py
@@ -1,27 +1,63 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from .models import Task
 
 # 03/12/2023 - Geoffroy
-# Vues pour la To-Do.
-# Liste, Ajout, Toggle (fait/pas fait), Suppression.
-# C'est le CRUD de base du développeur web. Le "katas" quotidien.
+# TP2 V1 - Vues améliorées avec messages et filtres
 
 def index(request):
-    tasks = Task.objects.all().order_by('-created_at')
+    filter_type = request.GET.get('filter', 'all')
+    priority = request.GET.get('priority')
+    
+    tasks = Task.objects.all()
+    
+    # Filtres
+    if filter_type == 'completed':
+        tasks = tasks.filter(completed=True)
+    elif filter_type == 'todo':
+        tasks = tasks.filter(completed=False)
+    elif filter_type == 'overdue':
+        from django.utils import timezone
+        tasks = tasks.filter(completed=False, due_date__lt=timezone.now().date())
+    
+    if priority:
+        tasks = tasks.filter(priority=int(priority))
+    
+    # Création
     if request.method == 'POST':
-        title = request.POST.get('title')
+        title = request.POST.get('title', '').strip()
+        priority = request.POST.get('priority', 3)
+        due_date = request.POST.get('due_date') or None
+        
         if title:
-            Task.objects.create(title=title)
+            Task.objects.create(title=title, priority=priority, due_date=due_date)
+            messages.success(request, f"Tâche '{title}' ajoutée !")
+        else:
+            messages.error(request, "Le titre est obligatoire")
         return redirect('index')
-    return render(request, 'todo/index.html', {'tasks': tasks})
+    
+    context = {
+        'tasks': tasks,
+        'filter': filter_type,
+        'priorities': Task.PRIORITY_CHOICES,
+        'stats': {
+            'total': Task.objects.count(),
+            'completed': Task.objects.filter(completed=True).count(),
+            'pending': Task.objects.filter(completed=False).count(),
+        }
+    }
+    return render(request, 'todo/index.html', context)
 
 def toggle_task(request, task_id):
-    task = Task.objects.get(pk=task_id)
+    task = get_object_or_404(Task, pk=task_id)
     task.completed = not task.completed
     task.save()
+    status = "terminée" if task.completed else "réouverte"
+    messages.info(request, f"Tâche {status}")
     return redirect('index')
 
 def delete_task(request, task_id):
-    task = Task.objects.get(pk=task_id)
+    task = get_object_or_404(Task, pk=task_id)
     task.delete()
+    messages.success(request, "Tâche supprimée")
     return redirect('index')
