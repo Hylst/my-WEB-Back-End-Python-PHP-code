6a00a268cf4661453ff1e45581c1ab0d75deb205
Geoffroy Streit <geoffroy.streit@gmail.com>
2023-12-04 22:00:00 +0100

    Upgrade TP2 V2 : Tags, sous-tâches et dashboard

--- Python_web/12_tp2_django_todolist/v2/todo/models.py
+++ Python_web/12_tp2_django_todolist/v2/todo/models.py
@@ -1,21 +1,75 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # 04/12/2023 - Geoffroy
-# TP2 - Version 2.
-# On ajoute une date d'échéance.
-# C'est comme un compte à rebours avant l'explosion. Ça motive.
+# TP2 V2 - Modèle avancé avec sous-tâches et tags
+
+class Tag(models.Model):
+    """Tags pour catégoriser les tâches."""
+    name = models.CharField(max_length=50, unique=True)
+    color = models.CharField(max_length=7, default="#3498db")  # Hex color
+    
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
+    PRIORITY_CHOICES = [
+        (1, '🔴 Critique'),
+        (2, '🟠 Haute'),
+        (3, '🟡 Moyenne'),
+        (4, '🟢 Basse'),
+    ]
+    
+    STATUS_CHOICES = [
+        ('todo', 'À faire'),
+        ('in_progress', 'En cours'),
+        ('done', 'Terminé'),
+        ('cancelled', 'Annulé'),
+    ]
+    
     title = models.CharField(max_length=200)
-    completed = models.BooleanField(default=False)
+    description = models.TextField(blank=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='todo')
+    priority = models.IntegerField(choices=PRIORITY_CHOICES, default=3)
+    tags = models.ManyToManyField(Tag, blank=True, related_name='tasks')
+    
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     due_date = models.DateField(null=True, blank=True)
-
+    completed_at = models.DateTimeField(null=True, blank=True)
+    
+    # Pour les sous-tâches
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='subtasks')
+    
+    class Meta:
+        ordering = ['status', 'priority', 'due_date', '-created_at']
+    
+    def __str__(self):
+        return self.title
+    
+    @property
     def is_overdue(self):
-        if self.due_date and not self.completed:
+        if self.due_date and self.status not in ['done', 'cancelled']:
             return self.due_date < timezone.now().date()
         return False
-
-    def __str__(self):
-        return f"{self.title} ({'Terminé' if self.completed else 'En cours'})"
+    
+    @property
+    def completion_percentage(self):
+        """Pourcentage de sous-tâches terminées."""
+        subtasks = self.subtasks.all()
+        if not subtasks:
+            return 100 if self.status == 'done' else 0
+        done = subtasks.filter(status='done').count()
+        return int((done / subtasks.count()) * 100)
+    
+    def complete(self):
+        self.status = 'done'
+        self.completed_at = timezone.now()
+        self.save()
+    
+    def reopen(self):
+        self.status = 'todo'
+        self.completed_at = None
+        self.save()
--- Python_web/12_tp2_django_todolist/v2/todo/views.py
+++ Python_web/12_tp2_django_todolist/v2/todo/views.py
@@ -1,39 +1,100 @@
-from django.shortcuts import render, redirect
-from .models import Task
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q, Count
+from .models import Task, Tag
 
 # 04/12/2023 - Geoffroy
-# V2 : On filtre et on trie.
-# J'ai ajouté des filtres pour afficher "Tout", "A faire", "Terminé".
-# C'est un peu plus UX friendly. On se sent moins submergé par la liste .
+# TP2 V2 - Dashboard avec statistiques et kanban-style
+
+def dashboard(request):
+    """Vue principale style kanban."""
+    tasks_by_status = {
+        'todo': Task.objects.filter(status='todo', parent__isnull=True),
+        'in_progress': Task.objects.filter(status='in_progress', parent__isnull=True),
+        'done': Task.objects.filter(status='done', parent__isnull=True),
+    }
+    
+    # Statistiques
+    stats = {
+        'total': Task.objects.filter(parent__isnull=True).count(),
+        'overdue': sum(1 for t in Task.objects.filter(parent__isnull=True) if t.is_overdue),
+        'completed_today': Task.objects.filter(
+            completed_at__date=timezone.now().date()
+        ).count() if 'timezone' in dir() else 0,
+    }
+    
+    tags = Tag.objects.annotate(task_count=Count('tasks'))
+    
+    return render(request, 'todo/dashboard.html', {
+        'tasks_by_status': tasks_by_status,
+        'stats': stats,
+        'tags': tags,
+        'priorities': Task.PRIORITY_CHOICES,
+    })
 
 def index(request):
-    filter_type = request.GET.get('filter', 'all')
+    """Liste classique avec filtres avancés."""
+    tasks = Task.objects.filter(parent__isnull=True)
     
+    # Filtres
+    status = request.GET.get('status')
+    priority = request.GET.get('priority')
+    tag = request.GET.get('tag')
+    search = request.GET.get('q')
+    
+    if status:
+        tasks = tasks.filter(status=status)
+    if priority:
+        tasks = tasks.filter(priority=int(priority))
+    if tag:
+        tasks = tasks.filter(tags__name=tag)
+    if search:
+        tasks = tasks.filter(Q(title__icontains=search) | Q(description__icontains=search))
+    
+    # Création rapide
     if request.method == 'POST':
-        title = request.POST.get('title')
-        date_str = request.POST.get('due_date')
+        title = request.POST.get('title', '').strip()
         if title:
-            # On gère le cas où la date est vide
-            due_date = date_str if date_str else None
-            Task.objects.create(title=title, due_date=due_date)
+            priority = request.POST.get('priority', 3)
+            due_date = request.POST.get('due_date') or None
+            Task.objects.create(title=title, priority=priority, due_date=due_date)
+            messages.success(request, f"✅ '{title}' ajouté")
         return redirect('index')
+    
+    return render(request, 'todo/index.html', {
+        'tasks': tasks,
+        'tags': Tag.objects.all(),
+        'priorities': Task.PRIORITY_CHOICES,
+        'statuses': Task.STATUS_CHOICES,
+    })
 
-    if filter_type == 'completed':
-        tasks = Task.objects.filter(completed=True).order_by('-created_at')
-    elif filter_type == 'todo':
-        tasks = Task.objects.filter(completed=False).order_by('due_date')
+def update_status(request, task_id, new_status):
+    """Change le statut d'une tâche (pour drag & drop)."""
+    task = get_object_or_404(Task, pk=task_id)
+    if new_status == 'done':
+        task.complete()
     else:
-        tasks = Task.objects.all().order_by('completed', 'due_date') # Non terminées en premier
-
-    return render(request, 'todo/index.html', {'tasks': tasks, 'filter': filter_type})
-
-def toggle_task(request, task_id):
-    task = Task.objects.get(pk=task_id)
-    task.completed = not task.completed
-    task.save()
+        task.status = new_status
+        task.save()
+    messages.info(request, f"Tâche mise à jour")
     return redirect('index')
 
 def delete_task(request, task_id):
-    task = Task.objects.get(pk=task_id)
+    task = get_object_or_404(Task, pk=task_id)
+    title = task.title
     task.delete()
+    messages.success(request, f"'{title}' supprimé")
     return redirect('index')
+
+def add_subtask(request, parent_id):
+    """Ajoute une sous-tâche."""
+    parent = get_object_or_404(Task, pk=parent_id)
+    if request.method == 'POST':
+        title = request.POST.get('title', '').strip()
+        if title:
+            Task.objects.create(title=title, parent=parent, priority=parent.priority)
+            messages.success(request, "Sous-tâche ajoutée")
+    return redirect('index')
+
+# Import manquant
+from django.utils import timezone
